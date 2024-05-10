b9a4412c8b5c30fdfdbdee14dee88af846783fc2
Emmanuel Blanco <ef.blanco@uniandes.edu.co>
2024-05-09 20:18:14 -0500

    Avance en prueba ingresar pieza

--- Proyecto/src/tests/TestsAdmin.java
+++ Proyecto/src/tests/TestsAdmin.java
@@ -1,7 +1,79 @@
 package tests;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestsAdmin {
+import Logica.Galeria;
+import Logica.Inventario;
+import Persistencia.InicializadorDeClases;
+import Logica.Administrador;
+import Logica.Pieza;
+import Logica.Escultura;
+import Logica.Fotografia;
+import Logica.Pintura;
+import Logica.Video;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+public class TestsAdmin 
+{
+    private Galeria GaleriaPrueba;
+    private ArrayList<String> Autores_uno;
+    private Escultura EsculturaPrueba;
+    private ArrayList<String> Autores_dos;
+    private Video VideoPrueba;
+    private Fotografia Photo;
+    private Pintura PinturaPrueba;
+    private ArrayList<Integer> Valores_uno;
+    private ArrayList<Integer> Valores_dos;
+    private ArrayList<Map<String,Object>> historial1;
+    private ArrayList<Map<String,Object>> historial2;
+    
+
+
+    @BeforeAll
+    void setUp() throws Exception
+    {
+        GaleriaPrueba = InicializadorDeClases.cargarGaleria();
+
+        Autores_uno = new ArrayList<String>();
+        Autores_uno.add("Autor1");
+        Autores_dos = new ArrayList<String>();
+        Autores_dos.add("Autor1");
+        Autores_dos.add("Autor2");
+        Valores_uno = new ArrayList<Integer>();
+        Valores_uno.add(120000);
+        Valores_dos = new ArrayList<Integer>();
+        Valores_dos.add(0);
+        Map<String,Object> propietario1 = new HashMap<String,Object>();
+        propietario1.put("loginPropietario","loginX");
+        propietario1.put("valorCompra",120000);
+        propietario1.put("fecha","02/20/18");
+        historial1.add(propietario1);
+        Map<String,Object> propietario2 = new HashMap<String,Object>();
+        propietario2.put("loginPropietario","loginY");
+        propietario2.put("valorCompra",340000);
+        propietario2.put("fecha","01/30/20");
+        historial2.add(propietario1);
+        historial2.add(propietario2);
+
+        EsculturaPrueba = new Escultura("El pensador", "Me", 1990, "somewhere", Autores_dos, true, -1, false, Valores_uno, true, "escultura", historial1, 1, 10, 12, 56, false);
+        VideoPrueba = new Video("Order 66", "Canc1ll3r_Palpatine", 2020, "In a galaxy far, far away", Autores_uno, false, 20, true, Valores_dos, false, "video", historial2, 20, 1, "720");
+        Photo = new Fotografia("Random photo", "Me_again", 2023, "who knows", Autores_uno, false, 32, true, Valores_dos, false, "video", historial1, "None", 28);
+        PinturaPrueba = new Pintura("Una pintura", "alguien", 1020, "???", Autores_dos, true, -1, false, Valores_uno, true, "pintura", historial2, 3, 3, 14, "creatividad");
+
+    }
+
+    @Test
+    void testIngresarPieza()
+    {
+        int tamanio_inicial = (GaleriaPrueba.getInventario().getPiezasBodega().size())+(GaleriaPrueba.getInventario().getPiezasExhibidad().size())
+        assertEquals(tamanio_inicial,GaleriaPrueba.getCantidadObras(), "La cantidad incial de obras no es correcta");
+        Administrador.ingresarPieza(GaleriaPrueba,VideoPrueba);
+        assertEquals(tamanio_inicial+1,GaleriaPrueba.getCantidadObras(), "La pieza no se agregó a la galería");
+
+    }
 }
