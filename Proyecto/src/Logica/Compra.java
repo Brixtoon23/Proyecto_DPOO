39f6f73d2ed72843eb9efadb2218445463e5e02c
Your Name <nk.rodriguez@unaindes.edu.co>
2024-06-02 02:16:46 -0500

    .

--- Proyecto/src/Logica/Compra.java
+++ Proyecto/src/Logica/Compra.java
@@ -6,106 +6,67 @@ public class Compra
 	private String compradorLogin;
 	private int precio;
 	private  String Nombrepieza;
-	private MetodoPago metodoPago; 
+	private String metodoPago; 
 	private String fecha;
 
-	
-
-
-	
-	
-	
-
-	
-
-	public Compra(String compradorLogin, int precio, String nombrepieza, MetodoPago metodoPagoObjeto, String fecha) {
+	public Compra(String compradorLogin, int precio, String nombrepieza, String metodoPago, String fecha) {
 		this.compradorLogin = compradorLogin;
 		this.precio = precio;
 		Nombrepieza = nombrepieza;
-		this.metodoPago = metodoPagoObjeto;
+		this.metodoPago = metodoPago;
 		this.fecha = fecha;
 	}
 
-
-
+	public String getCompradorLogin() {
+		return compradorLogin;
+	}
 
 	public void setCompradorLogin(String compradorLogin) {
 		this.compradorLogin = compradorLogin;
 	}
 
-
-
+	public int getPrecio() {
+		return precio;
+	}
 
 	public void setPrecio(int precio) {
 		this.precio = precio;
 	}
 
-
-
-
 	public String getNombrepieza() {
 		return Nombrepieza;
 	}
-
-
-
 
 	public void setNombrepieza(String nombrepieza) {
 		Nombrepieza = nombrepieza;
 	}
 
+	public String getMetodoPago() {
+		return metodoPago;
+	}
 
-
-
-	
-
-
-
+	public void setMetodoPago(String metodoPago) {
+		this.metodoPago = metodoPago;
+	}
 
 	public String getFecha() {
 		return fecha;
 	}
 
-
-
-
 	public void setFecha(String fecha) {
 		this.fecha = fecha;
 	}
-
-
-
-
-	public int getPrecio() {
-		return precio;
-	}
 	
+
 	
-	
-	public String getCompradorLogin() {
-		return compradorLogin;
-	}
 
-
-
-
-	public MetodoPago getMetodoPago() {
-		return metodoPago;
-	}
-
-
-
-
-	public void setMetodoPago(MetodoPago metodoPago) {
-		this.metodoPago = metodoPago;
-	}
 
 	
 	
 	
 
-
 	
+
 	
 	
 
